99e0b0351170ecbf4ce2bc1ec543f910c8defede
Ananda Muhammad <anandamuhammad@proton.me>
2024-10-05 10:38:47 +0800

    Complete the code logic to play the entire game

--- script.js
+++ script.js
@@ -11,5 +11,43 @@ const getComputerChoice = () => {
 const getHumanChoice = () => {
   const answer = prompt(`Please choose either "rock", "paper" or "scissors": `);
 
-  console.log(answer);
+  return answer;
 };
+
+const playGame = () => {
+  const playRound = (humanChoice, computerChoice) => {
+    if (humanChoice === "rock" && computerChoice === "paper") {
+      console.log(`You lose! Paper beats Rock`);
+      computerScore += 1;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      console.log(`You lose! Scissors beats Paper`);
+      computerScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+      console.log(`You lose! Rock beats Scissors`);
+      computerScore += 1;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log(`You win! Paper beats Rock`);
+      humanScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      console.log(`You win! Scissors beats Paper`);
+      humanScore += 1;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      console.log(`You win! Rock beats Scissors`);
+      humanScore += 1;
+    } else {
+      console.log("Draw!");
+    }
+  };
+
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+
+  playRound(humanSelection, computerSelection);
+  console.log(`human score: ${humanScore}, comp score: ${computerScore}`);
+};
+
+for (let i = 0; i < 5; i++) {
+  setTimeout(() => {
+    playGame();
+  }, 2000);
+}
